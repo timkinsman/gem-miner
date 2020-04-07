ac1a7fe4abac2fdc8559c4c78c77e7f295e174d8
timkinsman <tkinsm@gmail.com>
2020-04-07 19:25:24 +1000

    Working, some kinks

--- gem-miner.rb
+++ gem-miner.rb
@@ -1,5 +1,13 @@
+# TODO: check if successful login
+# TODO: Refactor to avoid suggested actions clash
+
 require 'nokogiri'
 require 'mechanize'
+require 'csv'
+
+if ARGV.length != 2
+    abort("usage: ruby ./gem-miner _login _password")
+end
 
 agent = Mechanize.new
 
@@ -7,19 +15,60 @@ agent = Mechanize.new
 
 login = agent.get('https://github.com/login')
 login_form = login.forms.first
-login_form.field_with(:name => 'login').value="timkinsman"
-login_form.field_with(:name => 'password').value="password"
+login_form.field_with(:name => 'login').value=ARGV[0]
+login_form.field_with(:name => 'password').value=ARGV[1]
 page = agent.submit login_form
 
 # Below will print page showing information confirming that you have logged in.
 
-#pp page
+category = [
+    "api-management",
+    "chat",
+    "code-quality",
+    "code-review",
+    "mobile-ci",
+    "container-ci",
+    "dependency-management",
+    "deployment",
+    "ides",
+    "learning",
+    "localization",
+    "mobile",
+    "monitoring",
+    "project-management",
+    "publishing",
+    "recently-added",
+    "security",
+    "support",
+    "testing",
+    "utilities"
+]
 
-#print agent.get('https://github.com/marketplace?category=code-quality&type=actions').search("h3.h4")[16].text
-#print agent.get('https://github.com/marketplace?category=code-quality&type=actions').search("span.text-small")[16].text
+for cat in category
+    CSV.open("#{cat}.csv", "w") do |csv|
+        csv << ["Action", "Stars"]
+        page = agent.get("https://github.com/marketplace?category=#{cat}&type=actions")
 
-# Begin
-pp agent.get('https://github.com/marketplace?category=code-quality&type=actions').search("div.px-3")[2].text
+        # First page
 
-# End
-pp agent.get('https://github.com/marketplace?category=code-quality&type=actions').search("div.px-3")[21].text
+        for i in 2..(page.search("div.px-3").length - 1)
+            if i > 21
+                break
+            end        
+            csv << [page.search("div.px-3")[i].search("h3.h4").text, page.search("div.px-3")[i].search("span.text-small").text.split.first]
+        end
+
+        # Subsequent pages
+
+        while page.link_with(text: 'Next')
+            link = page.link_with(text: 'Next')
+            page = link.click
+            for i in 2..(page.search("div.px-3").length - 1)
+                if i > 21
+                    break
+                end
+                csv << [page.search("div.px-3")[i].search("h3.h4").text, page.search("div.px-3")[i].search("span.text-small").text.split.first]
+            end
+        end
+    end
+end
